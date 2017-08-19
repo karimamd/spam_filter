bc011be34f6128f80c7bb789d5f0703a3d0668c2
oddcoder <ahmedsoliman@oddcoder.com>
2017-08-19 23:36:07 +0200

    This is smarter

--- remove_header.py
+++ remove_header.py
@@ -1,9 +1,6 @@
-def remove_header (email):
-    subject_idx = email.find("Subject")
-    email = email[subject_idx:]
-    subject = email[:email.find("\n")+1]
-    email.strip(subject)
-    message_id_x = email.find("Date:")
-    email = email[message_id_x:]
-
-    return subject + email[email.find("\n")+1:]
+import email
+import nltk
+def feature_extraction (email_text):
+    b = email.message_from_text(email_text)
+    return b.payload()
+    
