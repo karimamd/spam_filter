2891f4c21b0591647e4389237af38583214ee067
oddcoder <ahmedsoliman@oddcoder.com>
2017-08-23 13:43:08 +0200

    Fixing bugs

--- prediction_function.py
+++ prediction_function.py
@@ -6,40 +6,44 @@ from features import *
 from collections import Counter
 import  os.path
 import sys
-
-def word_spam_probability(word,spamCounter):
+eham,hham,spam,ehamCounter,hhamCounter,spamCounter=remove_big_words_from_list()
+ham = eham
+hamCounter = ehamCounter
+counter = ehamCounter + spamCounter
+def word_spam_probability(word):
 
     #TODO implement a better and faster search
+    probability = 1.0 / (counter+1)
     for i in spam:
         if i[0]==word:
             if i[1]!= 0:
                 probability=int(i[1])*1.0/spamCounter
-                #print(i[0]+" spam "+str(abs(log10(probability))))
-            else:
-                probability=1.0/(spamCounter+1)
-        else:
-            probability = 1.0 / (spamCounter+1)
-
+                break;
     return probability
 
-def word_ham_probability(word,ehamCounter,hhamCounter):
+def word_ham_probability(word):
     # TODO implement a better and faster search
     #TODO check for improvements on using eham or hham or both
-    hamCounter=ehamCounter+hhamCounter
-    ham=eham+hham
+    probability = 1.0 / (counter+1)
     for i in ham:
-        if i[0] == word:
-            if i[1] != 0:
+        if i[0] == word and i[1] != 0:
                 probability = int(i[1]) * 1.0 / hamCounter
-            else:
-                probability = 1.0 / (hamCounter+1)
-        else:
-            probability = 1.0 / (hamCounter+1)
+                break;
 
     return probability
 
 
-eham,hham,spam,ehamCounter,hhamCounter,spamCounter=remove_big_words_from_list()
+def classify_stemmed_text(txt):
+    pham = 0    # on log scale
+    pspam = 0   # on log scale
+    for word in txt.split(" "):
+        pspam -= log10(word_spam_probability(word))
+        pham -= log10(word_ham_probability(word))
+    if pham < pspam:
+        return "HAM"
+    else:
+        return "SPAM"
+
 '''
 #sort eham,ham and spam to easily find what we are looking for
 eham.sort()
@@ -47,58 +51,25 @@ hham.sort()
 spam.sort()
 
 '''
-
-probability_spam=1
-probability_ham=1
-#mail_words is list of stemmed words in fount in the input mail
-#TODO choose proper input method for email , a file for example
-mail="mail hundr free"
-
-
-parsed_email=email_parser(mail)
-body_dictionary=lemmatize_string(parsed_email["body"])
-mail_words=body_dictionary.split(" ")
-print(mail_words)
-
-
-for each_word in mail_words:
-    probability_spam*=word_spam_probability(each_word,spamCounter)
-    probability_ham*=word_ham_probability(each_word,ehamCounter,hhamCounter)
-#we use less than for spam because we take log so the one with the lowest power will be the smallest
-if abs(log10(probability_spam)) < abs(log10(probability_ham)):
-    print("spam mail")
-else:
-    print ("non-spam mail")
-
-
-#test
-#user input directory name containing files of testing (mails)
-directory = input("Directory: ")
-spam_mails=0
-ham_mails=0
-total=0
-for root, _, files in os.walk(directory):
-    for file_obj in files:
-        sys.stdout.write(".")
-        sys.stdout.flush()
-        total+=1
-        file_name = os.path.join(root, file_obj)
-        # open each file in directory and read them
-        with open(file_name, errors="replace") as f:
-            mail = f.read()
-        parsed_email = email_parser(mail)
-        body_dictionary = lemmatize_string(parsed_email["body"])
-        mail_words = body_dictionary.split(" ")
-        probability_spam = 1
-        probability_ham = 1
-        for each_word in mail_words:
-            probability_spam *= word_spam_probability(each_word, spamCounter)
-            probability_ham *= word_ham_probability(each_word, ehamCounter, hhamCounter)
-        # we use less than for spam because we take log so the one with the lowest power will be the smallest
-        if probability_spam < probability_ham:
-            spam_mails+=1
-        else:
-            ham_mails+=1
-
-print("ham : "+ str(1.0*ham_mails/total))
-print("spam: " + str(1.0*spam_mails/total))
+if __name__ == '__main__':
+    directory = input("Directory: ")
+    spam_mails=0
+    ham_mails=0
+    total=0
+    for root, _, files in os.walk(directory):
+        for file_obj in files:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+            total+=1
+            file_name = os.path.join(root, file_obj)
+            # open each file in directory and read them
+            with open(file_name, errors="replace") as f:
+                mail = f.read()
+            parsed_email = email_parser(mail)
+            body_txt = lemmatize_string(parsed_email["body"])
+            if classify_stemmed_text(body_txt) == "SPAM":
+                spam_mails+=1
+            else:
+                ham_mails+=1
+    print("ham : "+ str(1.0*ham_mails/total))
+    print("spam: " + str(1.0*spam_mails/total))
--- probability_tables.py
+++ probability_tables.py
@@ -1,34 +1,25 @@
 from spam import *
 from hham import *
 from eham import *
-#words > 14 charachters are probably non english meaningful  words
+#words > 9 charachters are probably non english meaningful  words
 #and counts number of all words(excluding filtered ones) in spam/eham/hham mails (summation of occurrences of all words)
 def remove_big_words_from_list():
     ehamCounter,hhamCounter,spamCounter=0,0,0
     for i in eham:
-        if len(i[0]) > 14:
+        if len(i[0]) > 9 or len(i[0]) == 1:
             eham.remove(i)
         else:
             ehamCounter+=int(i[1])
 
     for i in spam:
-        if len(i[0]) > 14:
+        if len(i[0]) > 9 or len(i[0]) == 1:
             spam.remove(i)
         else:
             spamCounter+=int(i[1])
 
     for i in hham:
-        if len(i[0]) > 14:
+        if len(i[0]) > 9 or len(i[0]) == 1:
             hham.remove(i)
         else:
             hhamCounter+=int(i[1])
     return eham,hham,spam,ehamCounter,hhamCounter,spamCounter
-
-
-
-
-
-
-
-
-
